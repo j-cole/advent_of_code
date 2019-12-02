4bd92844906f27f7ce1d3dcd95cf36d064874940
Jon Cole <jon.ed.cole@gmail.com>
2019-12-02 19:55:46 +0100

    Solution Day 1 - 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    day_1_1();
+}
+
+fn day_1_1() {
+    let file = File::open("input/2019/day_1_1.txt")
+        .expect("Could not open input file.");
+    let reader = BufReader::new(file);
+    let result: u64 = reader
+        .lines()
+        .filter_map(|l| l.ok())
+        .filter_map(|s| s.parse::<u64>().ok())
+        .map(|u| fuel_for_module(u))
+        .sum();
+    println!("Sum of fuel requirements: {}", result);
+}
+
+fn fuel_for_module(mass: u64) -> u64 {
+    mass / 3 - 2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(fuel_for_module(12), 2);
+    }
+
+    #[test]
+    fn example_2() {
+        assert_eq!(fuel_for_module(14), 2);
+    }
+
+    #[test]
+    fn example_3() {
+        assert_eq!(fuel_for_module(1969), 654);
+    }
+
+    #[test]
+    fn example_4() {
+        assert_eq!(fuel_for_module(100756), 33583);
+    }
 }
