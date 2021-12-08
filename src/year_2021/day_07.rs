36a5763ecaa904c126bcac2f142d55967a4776ce
Jon Cole <jon.ed.cole@gmail.com>
2021-12-08 19:33:45 +0100

    Initial answer 2021/07/2
    Note: this is a brute force solution without optimizations and therefore
    quite slow...

--- src/year_2021/day_07.rs
+++ src/year_2021/day_07.rs
@@ -8,6 +8,14 @@ pub fn part_1() -> i64 {
     part_1::find_min_fuel_required(&crabs)
 }
 
+#[allow(dead_code)]
+pub fn part_2() -> i64 {
+    let input =
+        fs::read_to_string("input/year_2021/day_07_1.txt").expect("Could not read input file.");
+    let crabs = parse_input(&input);
+    part_2::find_min_fuel_required(&crabs)
+}
+
 fn parse_input(input: &str) -> Vec<i64> {
     input
         .trim()
@@ -42,6 +50,49 @@ mod part_1 {
     }
 }
 
+mod part_2 {
+
+    pub fn find_min_fuel_required(crabs: &[i64]) -> i64 {
+        (0..=crabs.len() as i64)
+            .map(|crab| total_fuel_required(crabs, crab))
+            .min()
+            .unwrap_or(0)
+    }
+
+    fn total_fuel_required(crabs: &[i64], destination: i64) -> i64 {
+        crabs
+            .iter()
+            .fold(0, |acc, elem| fuel_required(*elem, destination) + acc)
+    }
+
+    fn fuel_required(crab: i64, destination: i64) -> i64 {
+        (0..=i64::abs(destination - crab)).sum::<i64>()
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn check_fuel_required() {
+            assert_eq!(fuel_required(16, 5), 66);
+        }
+
+        #[test]
+        fn check_total_fuel_required() {
+            let crabs: Vec<i64> = vec![16, 1, 2, 0, 4, 2, 7, 1, 2, 14];
+            assert_eq!(total_fuel_required(&crabs, 5), 168);
+            assert_eq!(total_fuel_required(&crabs, 2), 206);
+        }
+
+        #[test]
+        fn check_example_input() {
+            let crabs: Vec<i64> = vec![16, 1, 2, 0, 4, 2, 7, 1, 2, 14];
+            assert_eq!(find_min_fuel_required(&crabs), 168);
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -56,5 +107,10 @@ mod tests {
     #[test]
     fn check_answer_part_1() {
         assert_eq!(part_1(), 347509);
+    }
+
+    #[test]
+    fn check_answer_part_2() {
+        assert_eq!(part_2(), 98257206);
     }
 }
