8610cbc4f9671680da3ee7bf13751c33f1409857
Jon Cole <jon.ed.cole@gmail.com>
2021-12-08 11:47:57 +0100

    Move part 1 data into own sub module

--- src/year_2021/day_06.rs
+++ src/year_2021/day_06.rs
@@ -5,9 +5,9 @@ pub fn part_1() -> i64 {
     let input =
         fs::read_to_string("input/year_2021/day_06_1.txt").expect("Could not read input file.");
     let fish = parse_input(&input);
-    let mut simulation = LanternfishSimulation { fish };
+    let mut simulation = part_1::LanternfishSimulation::new(&fish);
     simulation.run(80);
-    simulation.count() as i64
+    simulation.count()
 }
 
 fn parse_input(input: &str) -> Vec<i64> {
@@ -18,61 +18,92 @@ fn parse_input(input: &str) -> Vec<i64> {
         .collect()
 }
 
-struct LanternfishSimulation {
-    fish: Vec<i64>,
-}
+mod part_1 {
 
-impl LanternfishSimulation {
+    pub struct LanternfishSimulation {
+        fish: Vec<i64>,
+    }
 
-    fn step(&mut self) {
-        let mut count_new_fish = 0;
-        for f in &mut self.fish {
-            if *f == 0 {
-                *f = 6;
-                count_new_fish += 1;
-            } else {
-                *f -= 1;
+    impl LanternfishSimulation {
+        pub fn new(starting_fish: &[i64]) -> Self {
+            LanternfishSimulation {
+                fish: starting_fish.to_vec(),
             }
         }
-        self.fish.extend(vec![8; count_new_fish]);
-    }
 
-    fn run(&mut self, num_steps: i64) {
-        for _ in 0..num_steps {
-            self.step();
+        fn step(&mut self) {
+            let mut count_new_fish = 0;
+            for f in &mut self.fish {
+                if *f == 0 {
+                    *f = 6;
+                    count_new_fish += 1;
+                } else {
+                    *f -= 1;
+                }
+            }
+            self.fish.extend(vec![8; count_new_fish]);
+        }
+
+        pub fn run(&mut self, num_steps: i64) {
+            for _ in 0..num_steps {
+                self.step();
+            }
+        }
+
+        pub fn count(&self) -> i64 {
+            self.fish.len() as i64
         }
     }
 
-    fn count(&self) -> usize {
-        self.fish.len()
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn check_step() {
+            let input: Vec<i64> = vec![3, 4, 3, 1, 2];
+            let mut simulation = LanternfishSimulation::new(&input);
+            simulation.step();
+            assert_eq!(simulation.fish, vec![2, 3, 2, 0, 1]);
+            simulation.step();
+            assert_eq!(simulation.fish, vec![1, 2, 1, 6, 0, 8]);
+            simulation.step();
+            assert_eq!(simulation.fish, vec![0, 1, 0, 5, 6, 7, 8]);
+            simulation.step();
+            assert_eq!(simulation.fish, vec![6, 0, 6, 4, 5, 6, 7, 8, 8]);
+        }
+
+        #[test]
+        fn check_example_input_18_steps() {
+            let input: Vec<i64> = vec![3, 4, 3, 1, 2];
+            let mut simulation = LanternfishSimulation::new(&input);
+            simulation.run(18);
+            assert_eq!(simulation.count(), 26);
+        }
+
+        #[test]
+        fn check_example_input_80_steps() {
+            let input: Vec<i64> = vec![3, 4, 3, 1, 2];
+            let mut simulation = LanternfishSimulation::new(&input);
+            simulation.run(80);
+            assert_eq!(simulation.count(), 5934);
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    mod part_1 {
-        use super::super::*;
+    use super::*;
 
-        #[test]
-        fn parse_example_input() {
-            let input = "3,4,3,1,2";
-            let expected_output: Vec<i64> = vec![3, 4, 3, 1, 2];
-            assert_eq!(parse_input(input), expected_output);
-        }
+    #[test]
+    fn parse_example_input() {
+        let input = "3,4,3,1,2";
+        let expected_output: Vec<i64> = vec![3, 4, 3, 1, 2];
+        assert_eq!(parse_input(input), expected_output);
+    }
 
-        #[test]
-        fn check_example_input() {
-            let input: Vec<i64> = vec![3, 4, 3, 1, 2];
-            let mut simulation = LanternfishSimulation { fish: input };
-            simulation.step();
-            assert_eq!(simulation.fish, vec![2, 3, 2, 0, 1]);
-            simulation.step();
-            assert_eq!(simulation.fish, vec![1, 2, 1, 6, 0, 8]);
-        }
-
-        #[test]
-        fn check_answer() {
-            assert_eq!(part_1(), 374927);
-        }
+    #[test]
+    fn check_answer_part_1() {
+        assert_eq!(part_1(), 374927);
     }
 }
